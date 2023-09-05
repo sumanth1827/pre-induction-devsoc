65a0e6b6e77e390bb42f9fe67343267f62ca4481
Rohan-J123 <rohanjha@aol.in>
2023-09-05 21:46:06 +0530

    Changes to Obstacles

--- Pre-induction-game/Assets/Scripts/NC.cs
+++ Pre-induction-game/Assets/Scripts/NC.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Questions : MonoBehaviour
+public class NC : MonoBehaviour
 {
     public EnemyAI prof;
     public GameObject questions;
 
     public float speed;
-    public float maxY;
-    public float minY;
+    public float maxX;
+    public float minX;
     public float maxT;
     public float minT;
     public float timeBetweenSpawn;
@@ -29,9 +29,9 @@ public class Questions : MonoBehaviour
         if (prof != null) {
             if(prof.aggro){
                 timeBetweenSpawn = Random.Range(minT, maxT);
-                float randomY = Random.Range(minY, maxY);
-                var q = Instantiate(questions, transform.position + new Vector3(0, randomY, 0), transform.rotation);
-                q.GetComponent<Rigidbody2D>().velocity = transform.up * speed;
+                float randomX = Random.Range(minX, maxX);
+                var q = Instantiate(questions, transform.position + new Vector3(randomX, 0, 0), transform.rotation);
+                q.GetComponent<Rigidbody2D>().velocity = -transform.up * speed;
                 StartCoroutine(des(q));
             }
         }
