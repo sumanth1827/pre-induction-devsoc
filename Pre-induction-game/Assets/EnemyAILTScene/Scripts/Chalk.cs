85243ba6cb025e51b58bf21d230a35955641d00d
sumanth1827 <sumanth304sv@gmail.com>
2023-09-17 14:27:51 +0530

    LT scene pre-final

--- Pre-induction-game/Assets/EnemyAILTScene/Scripts/Chalk.cs
+++ Pre-induction-game/Assets/EnemyAILTScene/Scripts/Chalk.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chalk : MonoBehaviour {
+public class Chalk : MonoBehaviour
+{
 	public Vector3 targetPos;
 	public float speed = 10;
 	public float arcHeight = 1;
 	private Vector3 startPos;
-	
-	void Start() {
+
+	void Start()
+	{
 		startPos = transform.position;
 	}
-	
-	void Update() {
+
+	void Update()
+	{
 		// Compute the next position, with arc added in
 		float x0 = startPos.x;
 		float x1 = targetPos.x;
@@ -21,20 +24,22 @@ public class Chalk : MonoBehaviour {
 		float baseY = Mathf.Lerp(startPos.y, targetPos.y, (nextX - x0) / dist);
 		float arc = arcHeight * (nextX - x0) * (nextX - x1) / (-0.25f * dist * dist);
 		Vector3 nextPos = new Vector3(nextX, baseY + arc, transform.position.z);
-		
+
 		// Rotate to face the next position, and then move there
 		transform.rotation = LookAt2D(nextPos - transform.position);
 		transform.position = nextPos;
-		
+
 		// Do something when we reach the target
 		if (nextPos == targetPos) Arrived();
 	}
-	
-	void Arrived() {
+
+	void Arrived()
+	{
 		Destroy(gameObject);
 	}
-	
-	static Quaternion LookAt2D(Vector2 forward) {
+
+	static Quaternion LookAt2D(Vector2 forward)
+	{
 		return Quaternion.Euler(0, 0, Mathf.Atan2(forward.y, forward.x) * Mathf.Rad2Deg);
 	}
 }
---
+++ Pre-induction-game/Assets/enemyai.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using static UnityEngine.UI.Image;
+
+public class enemyai : MonoBehaviour
+{
+    GameObject player;
+    Animator anim;
+    float speed = 10f;
+    Rigidbody2D rb;
+    Vector2 distance;
+    SpriteRenderer sprite;
+    Transform throwpos;
+    bool isagro = false;
+
+    [SerializeField] GameObject chalk;
+    [SerializeField] Transform bounds;
+
+    
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        throwpos = GetComponentsInChildren<Transform>()[1];
+        StartCoroutine(behaviour(4f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distance = new Vector2(player.transform.position.x - transform.position.x, transform.position.y);
+
+
+
+        if (rb.velocity != Vector2.zero && !isagro)
+        {
+            anim.SetBool("attack", false);
+            anim.SetBool("walk", true);
+        }
+        else if(rb.velocity == Vector2.zero && !isagro)
+        {
+            anim.SetBool("walk", false);
+            anim.SetBool("attack", true);
+        }
+        else if(isagro)
+        {
+            anim.SetBool("walk", false);
+            anim.SetBool("attack", false);
+        }
+        if (distance.x > 0)
+        {
+            sprite.flipX = true;
+
+        }
+        else if (distance.y < 0)
+        {
+            sprite.flipX = false;
+
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (distance.x > 10f || distance.x < -10f)
+        {
+            //anim.SetBool("attack", false);
+            rb.velocity = new Vector2(distance.normalized.x * speed, 0f);
+
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+
+        }
+        if(isagro)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+    }
+    public void throw_chalk()
+        {
+        //Instantiate(chalk, throwpos.position, Quaternion.identity);
+        var c = Instantiate(chalk, throwpos.position, Quaternion.identity);
+        Chalk cb = c.GetComponent<Chalk>();
+        cb.targetPos = new Vector3(player.transform.position.x - (transform.position.x - player.transform.position.x), -5, 0);
+        cb.arcHeight = player.transform.position.y + 4;
+    }
+    private IEnumerator behaviour(float timer)
+    {
+        yield return new WaitForSeconds(timer);
+        
+        int time = Random.Range(2, 5);
+        yield return new WaitForSeconds(time);
+        isagro = true;
+        anim.SetTrigger("agro");
+        yield return new WaitForSeconds(2.5f);
+        isagro = false;
+        time = Random.Range(5, 10);
+        yield return new WaitForSeconds(time);
+        StartCoroutine(behaviour(1f));
+    }
+
+
+}
