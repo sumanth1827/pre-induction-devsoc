de7521991b44771617eefe17d23162f887fe125d
sumanth1827 <sumanth304sv@gmail.com>
2023-09-18 19:12:36 +0530

    fixed dogs

--- Pre-induction-game/Assets/dogenemy.cs
+++ Pre-induction-game/Assets/dogenemy.cs
@@ -7,36 +7,111 @@ public class dogenemy : MonoBehaviour
     GameObject player;
     Animator anim;
     Rigidbody2D rb;
-    float distance;
+    Vector2 distance;
+    [SerializeField] float speed = 17f;
+    SpriteRenderer sprite;
+    [SerializeField] float jumpforce = 15f;
+    float minchasedist = 25f;
+    float attackdistance = 10f;
 
-    float minchasedist = 20f;
-    float attackdistance = 5f;
+    bool attacked = false;
+    bool attackcooldown = false;
+    bool firsttime = true;
 
-    bool attacked;
-     
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        distance = new Vector2(player.transform.position.x - transform.position.x, transform.position.y);
+    }
 
 
     // Start is called before the first frame update
-    void Start()
-    {
-        player = GameObject.FindWithTag("Player");
-        anim = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        distance = player.transform.position.x - transform.position.x;
-
-
-        
-    }
-    private void FixedUpdate()
-    {
-        if(distance < minchasedist)
+        if (firsttime)
         {
+            minchasedist = 7f;
+            attackdistance = 1f;
+            
+        }
+        else
+        {
+            anim.SetBool("firsttime", true);
+            minchasedist = 25f;
+            attackdistance = 10f;
+        }
+        distance = new Vector2(player.transform.position.x - transform.position.x, transform.position.y);
+        if (distance.x > 0)
+        {
+            sprite.flipX = true;
 
         }
+        else if (distance.x < 0)
+        {
+            sprite.flipX = false;
+
+        }
+
+        
+
+    }
+    
+    private void FixedUpdate()
+    {
+        
+        
+        if (Mathf.Abs(distance.x) < minchasedist && Mathf.Abs(distance.x) > attackdistance && !attackcooldown)
+        {
+           
+               rb.velocity = new Vector2 (distance.normalized.x * speed, 0f);
+               anim.SetBool("isMoving", true);
+            
+                firsttime = false;
+        }
+        else if(Mathf.Abs(distance.x) <= attackdistance && !attackcooldown)
+        {
+            anim.SetBool("isMoving", false);
+            anim.SetTrigger("isAttacking");
+
+            attacked = true;
+            rb.velocity = new Vector2(distance.normalized.x * (speed + 5), rb.velocity.y);
+        }
+        else if(Mathf.Abs(distance.x) > minchasedist)
+        {
+            rb.velocity = Vector2.zero;
+            anim.SetBool("isMoving", false);
+        }
+        
+
+        
+        if(attacked)
+        {
+          
+            rb.AddForce(new Vector2(rb.velocity.x, jumpforce), ForceMode2D.Impulse);
+            attacked = false;
+            attackcooldown = true;
+            
+            StartCoroutine(coolattack());
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            //end
+        }
+    }
+    IEnumerator coolattack()
+    {
+
+        yield return new WaitForSeconds(0.5f);
+        attackcooldown = false;
     }
 }
