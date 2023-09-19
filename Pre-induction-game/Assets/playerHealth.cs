c0f8740af5952b549b4c57bfe8f6e4840beea7b3
sumanth1827 <sumanth304sv@gmail.com>
2023-09-19 13:52:54 +0530

    health2

--- Pre-induction-game/Assets/playerHealth.cs
+++ Pre-induction-game/Assets/playerHealth.cs
@@ -1,31 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Microlight.MicroBar;
+using UnityEngine.WSA;
 
-public class playerHealth : MonoBehaviour
+public class player_health : MonoBehaviour
 {
+    [SerializeField] MicroBar healthbar;
     public float health = 100f;
-    public GameObject healthBar;
+    public static player_health instance;
+    Animator anim;
+    [SerializeField] Animator canvasanim;
     // Start is called before the first frame update
+    private void Awake()
+    {
+
+        healthbar = GetComponentsInChildren<MicroBar>()[1];
+        healthbar.Initialize(100f);
+        anim = GetComponent<Animator>();
+    }
     void Start()
     {
-        
+        instance = this;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if(health >= 0){
-            healthBar.transform.localScale = new Vector3((health)/100f, 1, 1);
+        if (health > 0)
+        {
+            healthbar.UpdateHealthBar(health, true);
         }
-        else{
-            healthBar.transform.localScale = new Vector3(0, 1, 1);
-        }
-    }
+        else if (health <= 0)
+        {
+            anim.SetBool("death", true);
+            playermovement.instance.alive = false;
 
-    void OnTriggerEnter2D(Collider2D col){
-        if(col.tag == "Attack"){
-            health -= 10f;
         }
+
+    }
+    public void decreasehealth(float amount)
+    {
+        health -= amount;
+
+    }
+    public void canvasanimation()
+    {
+        canvasanim.SetBool("gameover", true);
+        anim.SetBool("death", false);
     }
 }
